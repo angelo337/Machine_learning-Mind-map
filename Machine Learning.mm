a682237f03460d21ec99c1f4e075ed2b618fa62e
Angelo Rodriguez <angelor337@hotmail.com>
2016-09-27 08:19:06 -0500

    Add files via upload

--- Machine Learning.mm
+++ Machine Learning.mm
@@ -1,6 +1,6 @@
 <map version="freeplane 1.3.0">
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
-<node TEXT="Machine Learning" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1474410365508"><hook NAME="MapStyle">
+<node TEXT="Machine Learning" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1474410365508"><hook NAME="MapStyle" zoom="0.9">
     <properties show_note_icons="true"/>
 
 <map_styles>
@@ -109,7 +109,6 @@
 <node TEXT="Supervised" ID="ID_1969183180" CREATED="1460469037185" MODIFIED="1460469041447">
 <node TEXT="Clasification" ID="ID_990138118" CREATED="1460469082995" MODIFIED="1460469086931">
 <node TEXT="Logistic Regresion" ID="ID_1511948008" CREATED="1460469087967" MODIFIED="1460469094648"/>
-<node TEXT="Suport Vector Machines (SVM)" ID="ID_744349480" CREATED="1460469095932" MODIFIED="1474410782934"/>
 <node TEXT="Tree-Based Methods" ID="ID_1356177590" CREATED="1474412437890" MODIFIED="1474412533773">
 <node TEXT="Decesion Tree" ID="ID_1227478902" CREATED="1460469110107" MODIFIED="1460469118509"/>
 <node TEXT="Random Forest" ID="ID_1737661010" CREATED="1460469121772" MODIFIED="1460469128713"/>
@@ -130,19 +129,213 @@
 </node>
 </node>
 <node TEXT="One R" ID="ID_1111484842" CREATED="1474411511883" MODIFIED="1474411539692"/>
-<node TEXT="Naive Bayes" ID="ID_124828153" CREATED="1474411544216" MODIFIED="1474411550640"/>
+<node TEXT="&lt;100k Samples" ID="ID_1353524827" CREATED="1474980488973" MODIFIED="1474980552095">
+<node TEXT="Yes" ID="ID_80571239" CREATED="1474980738026" MODIFIED="1474980744303">
+<node TEXT="Linear SVC" ID="ID_1947372607" CREATED="1474980567924" MODIFIED="1474981113988"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Classification
+    </p>
+    <p>
+      http://scikit-learn.org/stable/modules/svm.html#classification
+    </p>
+    <p>
+      SVC, NuSVC and LinearSVC are classes capable of performing multi-class classification on a dataset.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="&lt;-- Not Working" ID="ID_1759719609" CREATED="1474980197743" MODIFIED="1474980221737">
+<node TEXT="Text Data" ID="ID_983751453" CREATED="1474980602891" MODIFIED="1474980611889">
+<node TEXT="Yes" ID="ID_528621073" CREATED="1474980614760" MODIFIED="1474980619404">
+<node TEXT="Naive Bayes" ID="ID_1460049834" CREATED="1474980626280" MODIFIED="1474980634412"/>
+</node>
+<node TEXT="No" ID="ID_1753667762" CREATED="1474980679526" MODIFIED="1474980688873">
+<node TEXT="K-Neighbors Classifier" ID="ID_1166272259" CREATED="1474980621386" MODIFIED="1474980674991">
+<node TEXT="&lt;-- Not Working" ID="ID_515030127" CREATED="1474980699799" MODIFIED="1474980709686">
+<node TEXT="SVC" ID="ID_705987109" CREATED="1474980712860" MODIFIED="1474980717877"/>
+<node TEXT="Ensemble Classifiers" ID="ID_908506355" CREATED="1474980720026" MODIFIED="1474980729363"/>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="No" ID="ID_1871669997" CREATED="1474980750413" MODIFIED="1474980753415">
+<node TEXT="SGD Classifier" ID="ID_217211989" CREATED="1474980754868" MODIFIED="1474980764867">
+<node TEXT="&lt;-- Not Working" ID="ID_1474945847" CREATED="1474980785165" MODIFIED="1474980787098">
+<node TEXT="Kernel Approximation" ID="ID_1851409571" CREATED="1474980794367" MODIFIED="1474980808101"/>
+</node>
+</node>
+</node>
+</node>
 </node>
 </node>
 <node TEXT="Unsupervised" ID="ID_503978692" CREATED="1460469042697" MODIFIED="1460469046565">
 <node TEXT="Deep Learning" ID="ID_1179999235" CREATED="1460469052215" MODIFIED="1460469061944">
+<node TEXT="Preprosecing" ID="ID_924459926" CREATED="1474980512227" MODIFIED="1474980525129">
 <node TEXT="Embedding" ID="ID_1647407478" CREATED="1474410328776" MODIFIED="1474410333210">
 <node TEXT="GloVe" ID="ID_1638758682" CREATED="1460469173378" MODIFIED="1460469178236"/>
 <node TEXT="Doc2Vec" ID="ID_1773683696" CREATED="1460469154724" MODIFIED="1460469162238"/>
 <node TEXT="Word2Vec" ID="ID_1480827473" CREATED="1460469147659" MODIFIED="1460469153396"/>
 </node>
 </node>
+</node>
 <node TEXT="Clusters" ID="ID_930225835" CREATED="1460469073095" MODIFIED="1460469077481">
-<node TEXT="K-NN" ID="ID_717384098" CREATED="1474410394209" MODIFIED="1474410399756"/>
+<node TEXT="number of Categories Know" ID="ID_1805956192" CREATED="1474928931400" MODIFIED="1474928947846">
+<node TEXT="YES" ID="ID_1845627618" CREATED="1474928949894" MODIFIED="1474928952380">
+<node TEXT="10K Samples" ID="ID_1667981940" CREATED="1474928983138" MODIFIED="1474928998668">
+<node TEXT="YES +" ID="ID_1164927355" CREATED="1474929009847" MODIFIED="1474929099261">
+<node TEXT="Kmeans" ID="ID_773090384" CREATED="1474929015831" MODIFIED="1474980972068"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      K-means
+    </p>
+    <p>
+      http://scikit-learn.org/stable/modules/clustering.html#k-means
+    </p>
+    <p>
+      The KMeans algorithm clusters data by trying to separate samples in n groups of equal variance, minimizing a criterion known as the inertia or within-cluster sum-of-squares. This algorithm requires the number of clusters to be specified. It scales well to large number of samples and has been used across a large range of application areas in many different fields.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+<node TEXT="&lt;-- Not Working" ID="ID_109172990" CREATED="1474980197743" MODIFIED="1474980221737">
+<node TEXT="Spectral Clustering" ID="ID_1373430207" CREATED="1474929023166" MODIFIED="1474979964883"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Spectral clustering
+    </p>
+    <p>
+      http://scikit-learn.org/stable/modules/clustering.html#spectral-clustering
+    </p>
+    <p>
+      SpectralClustering does a low-dimension embedding of the affinity matrix between samples, followed by a KMeans in the low dimensional space. It is especially efficient if the affinity matrix is sparse and the pyamg module is installed. SpectralClustering requires the number of clusters to be specified. It works well for a small number of clusters but is not advised when using many clusters.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="GMM" ID="ID_917478405" CREATED="1474929035013" MODIFIED="1474980191984"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Gaussian mixture models classifier
+    </p>
+    <p>
+      http://scikit-learn.org/stable/modules/mixture.html
+    </p>
+    <p>
+      A Gaussian mixture model is a probabilistic model that assumes all the data points are generated from a mixture of a finite number of Gaussian distributions with unknown parameters. One can think of mixture models as generalizing k-means clustering to incorporate information about the covariance structure of the data as well as the centers of the latent Gaussians.
+    </p>
+    <p>
+      Scikit-learn implements different classes to estimate Gaussian mixture models, that correspond to different estimation strategies, detailed below.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="NO -" ID="ID_190256682" CREATED="1474929054677" MODIFIED="1474929105561">
+<node TEXT="MiniBatch KMeans" ID="ID_1630695855" CREATED="1474929060359" MODIFIED="1474980011944"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      Mini Batch K-Means
+    </p>
+    <p>
+      http://scikit-learn.org/stable/modules/clustering.html#mini-batch-k-means
+    </p>
+    <p>
+      The MiniBatchKMeans is a variant of the KMeans algorithm which uses mini-batches to reduce the computation time, while still attempting to optimise the same objective function. Mini-batches are subsets of the input data, randomly sampled in each training iteration. These mini-batches drastically reduce the amount of computation required to converge to a local solution. In contrast to other algorithms that reduce the convergence time of k-means, mini-batch k-means produces results that are generally only slightly worse than the standard algorithm.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="NO" ID="ID_498157716" CREATED="1474928953697" MODIFIED="1474928955811">
+<node TEXT="10K Samples" ID="ID_1863560379" CREATED="1474928983138" MODIFIED="1474928998668">
+<node TEXT="YES +" ID="ID_596932130" CREATED="1474929009847" MODIFIED="1474929099261">
+<node TEXT="MeanShift" ID="ID_1395428434" CREATED="1474929132472" MODIFIED="1474929322228"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      http://scikit-learn.org/stable/modules/generated/sklearn.cluster.MeanShift.html#sklearn.cluster.MeanShift
+    </p>
+    <p>
+      Mean shift clustering aims to discover &#8220;blobs&#8221; in a smooth density of samples. It is a centroid-based algorithm, which works by updating candidates for centroids to be the mean of the points within a given region. These candidates are then filtered in a post-processing stage to eliminate near-duplicates to form the final set of centroids.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+<node TEXT="VBGMM" ID="ID_1258634674" CREATED="1474929150968" MODIFIED="1474979806307"><richcontent TYPE="NOTE">
+
+<html>
+  <head>
+    
+  </head>
+  <body>
+    <p>
+      variational Gaussian mixtures&#182;
+    </p>
+    <p>
+      http://scikit-learn.org/stable/modules/generated/sklearn.mixture.VBGMM.html#sklearn.mixture.VBGMM
+    </p>
+    <p>
+      The VBGMM object implements a variant of the Gaussian mixture model with variational inference algorithms. The API is identical to GMM. It is essentially a middle-ground between GMM and DPGMM, as it has some of the properties of the Dirichlet process.
+    </p>
+  </body>
+</html>
+
+</richcontent>
+</node>
+</node>
+<node TEXT="NO -" ID="ID_1787171932" CREATED="1474929054677" MODIFIED="1474929105561">
+<node TEXT="MALA SUERTE" ID="ID_1885869455" CREATED="1474929225680" MODIFIED="1474929231505"/>
+</node>
+</node>
+</node>
+</node>
 </node>
 </node>
 </node>
